4db1c2e432ad10c09c978dea0d4761c91836c350
Navinder Gill <nnavigill784@gmail.com>
2025-11-29 18:29:04 +0530

    adding the main file for entity manager where the actual query runs using entityManager

--- jdbc-demo/src/main/java/org/example/JPADemoMain.java
+++ jdbc-demo/src/main/java/org/example/JPADemoMain.java
@@ -1,4 +1,29 @@
 package org.example;
 
+import jakarta.persistence.EntityManager;
+
 public class JPADemoMain {
+
+    static void main() {
+
+        EntityManager em = JAPUtil.getEntityManager();
+
+        try {
+            UserClassHibernate user = new UserClassHibernate("Navi" ,"gillnavi752@gmail.com" );
+
+            em.getTransaction().begin();
+
+            em.persist(user);
+
+            em.getTransaction().commit();
+
+            System.out.println("User Saved with id : " + user.getId() );
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            em.close();
+
+            JAPUtil.close();
+        }
+    }
 }
