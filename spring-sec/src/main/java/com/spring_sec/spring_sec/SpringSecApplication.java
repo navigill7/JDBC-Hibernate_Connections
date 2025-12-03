0c8de3c62b03f376683c77acc96fd459a11c72f8
Navinder Gill <nnavigill784@gmail.com>
2025-12-03 14:34:03 +0530

    adding the main file

---
+++ spring-sec/src/main/java/com/spring_sec/spring_sec/SpringSecApplication.java
@@ -0,0 +1,13 @@
+package com.spring_sec.spring_sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecApplication.class, args);
+	}
+
+}
